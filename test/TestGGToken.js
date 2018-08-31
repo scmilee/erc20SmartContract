ee6ff2a806053abafeb7c1405b502dee82bbd05f
scmilee <drudged@oregonstate.edu>
2018-08-31 15:19:48 -0700

    add test coverage surrounding the modifier addition to GGtokens Transfer function

--- test/TestGGToken.js
+++ test/TestGGToken.js
@@ -2,34 +2,72 @@
 var GGToken = artifacts.require("GGToken");
 
 contract('TestGGToken' , function() {
+  const CBT = web3.eth.accounts[0];
+  const learner = web3.eth.accounts[1];
+  const learnerTwo = web3.eth.accounts[2];
 
   it("should have 1000000000 GGTokens" , async() => {
     let instance = await GGToken.deployed();
-    const totalSupply = await instance.totalSupply.call();
-
+    
+    const totalSupply = await instance.totalSupply();
     const expected = 1000000000;
 
     assert.equal(totalSupply, expected);
   });
 
-  it("should make the owner have 1000000000 GGTokens initially.", async()=>{
+  it("should make CBT have 1000000000 GGTokens initially", async()=>{
     let instance = await GGToken.deployed();
-    const ownerBalance = await instance.balanceOf.call(0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222);
-    //make sure the default owner has all of the tokens
-    console.log(ownerBalance.toFixed())
+    //the CBTs addres hardcoded into the contract
+    const CBTBalance = await instance.balanceOf(CBT);
+    //make sure the default CBT has all of the tokens
+
     const expected = 1000000000;
-    assert.equal(ownerBalance.valueOf(), expected );
+    assert.equal(CBTBalance.toNumber(), expected );
   });
 
-  // it("should allow the owner to send tokens wherever they want.", async()=>{
-  //   let instance = await GGToken.deployed();
-  //   const sendGG = await instance.transfer(0xD99E32D217aBF4bf3da11E58A76a8231EFC2c469, 100000);
-  //   const recieverBalance = await instance.balanceOf(0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222);
+  it("should allow CBT to transfer tokens wherever they want", async()=>{
+    let instance = await GGToken.deployed();
+  
+    await instance.transfer(learner, 100000, {from: CBT});
+    const learnerBalance = await instance.balanceOf(learner);
  
-  //   const expected = 100000;
-  //   assert.equal(ownerBalance, expected );
-  // });
+    const expected = 100000;
+    assert.equal(learnerBalance.toNumber(), expected );
+  });
 
+  it("should allow a learner to transfer tokens back to CBT", async()=>{
+    let instance = await GGToken.deployed();
+    //send the learner account 100000 coins so we can test if they can send them back
+    await instance.transfer(learner, 100000, {from: CBT});
+    await instance.transfer(CBT, 100000, {from: learner});
+    
+    const learnerBalance = await instance.balanceOf(learner);
+    const CBTBalance = await instance.balanceOf(CBT);
 
+    const expectedLearnerBal = 0;
+    assert.equal(learnerBalance.toNumber(), expectedLearnerBal );
+    const expected = 1000000000;
+    assert.equal(CBTBalance.toNumber(), expected );
+
+  });
+
+  it("should block a learner from transfering tokens to any address except CBT's", async()=>{
+    let instance = await GGToken.deployed();
+    //send the learner account 100000 coins so we can test if they can send them back
+    await instance.transfer(learner, 100000, {from: CBT});
+    //try block because this function should fail every time
+    try {
+      await instance.transfer(learnerTwo, 100000, {from: learner})
+    } catch(e) {}
+    
+    const learnerBalance = await instance.balanceOf(learner);
+    const learnerTwoBalance = await instance.balanceOf(learnerTwo);
+
+    const expectedLearnerBal = 100000;
+    assert.equal(learnerBalance.toNumber(), expectedLearnerBal );
+    const expected = 0;
+    assert.equal(learnerTwoBalance.toNumber(), expected );
+
+  })
 
 })
