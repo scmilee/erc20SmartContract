ace1f9caa7cd252e7b928ad0f17c139e808bdf17
scmilee <drudged@oregonstate.edu>
2018-08-31 11:59:37 -0700

    set up solidity testing basics and the first modifier for the transafer function

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
-var HelloWorld = artifacts.require("./HelloWorld.sol");
+var GGToken = artifacts.require("./GGToken.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(HelloWorld);
+  deployer.deploy(GGToken);
 };
