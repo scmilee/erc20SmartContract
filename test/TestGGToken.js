d098916a4f5b887ede5ee290d8e278cb8d7de278
scmilee <drudged@oregonstate.edu>
2018-09-04 14:06:41 -0700

    add test coverage for the approve functions (approve and transferFrom) and the modifier placed on it to dissallow learners from giving other learners approval/the ability to transfer tokens from their address

--- test/TestGGToken.js
+++ test/TestGGToken.js
@@ -79,3 +79,55 @@ contract('GGToken transfer coverage' , function() {
 
   })
 });
+
+
+contract('GGToken approve/allowance coverage' , function() {
+  
+  const CBT = web3.eth.accounts[0];
+  const learner = web3.eth.accounts[1];
+  const learnerTwo = web3.eth.accounts[2];
+  let instance;
+  
+  beforeEach(async()=>{
+    instance = await GGToken.new({from: CBT});
+    //approve the learner account for 100000 coins
+    await instance.approve(learner, 100000, {from: CBT});
+  });
+
+  it("should allow CBT to give an allowance of tokens to whomever they want", async()=>{ 
+    const learnerAllowance = await instance.allowance(CBT,learner);
+    const expected = 100000;
+    assert.equal(learnerAllowance.toNumber(), expected );
+  });
+
+  it("should allow a learner to give an allowance of tokens to CBT", async()=>{
+    //transfer the approved coins to the learner then give cbt approval to do the same from the learner
+    await instance.transferFrom(CBT, learner ,100000, {from: learner});
+    await instance.approve(CBT, 100000, {from: learner});
+
+    const learnerAllowance = await instance.allowance(CBT ,learner);
+    const CBTAllowance = await instance.allowance(learner ,CBT);
+    const expectedLearnerAllownce = 0;
+    assert.equal(learnerAllowance.toNumber(), expectedLearnerAllownce );
+    const expected = 100000;
+    assert.equal(CBTAllowance.toNumber(), expected );
+  });
+
+  it("should block a learner from transfering tokens to any address except CBT's", async()=>{
+    //try block because this function should fail every time
+    await instance.transferFrom(CBT, learner ,100000, {from: learner});
+    try {
+      await instance.approve(learnerTwo, 100000, {from: learner})
+    } catch(e) {}
+    
+    const learnerBalance = await instance.balanceOf(learner);
+    const learnerTwoAllowance = await instance.allowance(learner,learnerTwo);
+
+    //Allowance should stay at 100k due to the previous transfer function failing
+    const expectedLearnerBal = 100000;
+    assert.equal(learnerBalance.toNumber(), expectedLearnerBal );
+    const expected = 0;
+    assert.equal(learnerTwoAllowance.toNumber(), expected );
+
+  });
+});
